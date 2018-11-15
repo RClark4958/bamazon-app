1b70f706d74feb744f8b23689c6ce0a7f0533ff0
Rclark4598 <rickdclark48@gmail.com>
2018-11-15 00:06:53 -0700

    adding_seeds

---
+++ sql/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity)
+VALUES ("Nintendo GameBoy", "Nintendo Hardware", 99.99, 50),
+    ("Sega DreamGear", "Sega Hardware", 149.99, 50),
+    ("Super Nintendo", "Nintendo Hardware", 149.99, 75),
+    ("Sega Dreamcast", "Sega Hardware", 199.99, 30),
+    ("Sony Playstation", "Sony Hardware", 249.99, 45),
+    ("Microsoft Xbox", "Microsoft Hardware", 199.99, 50),
+    ("GP32", "GamePark Hardware", 199.99, 55),
+    ("Virtual Boy", "Nintendo Hardware", 149.99, 20),
+    ("GameBoy Advance", "Nintendo Hardware", 99.99, 100),
+    ("PSP", "Sony Hardware", 199.99, 10);
+
+SELECT * FROM bamazon.products;
